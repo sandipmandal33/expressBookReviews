20b9f491084f366a0be06d0afe4dbe62c30af0ea
Sandip Kumar Mandal <sandipmandal33@hotmail.com>
2025-05-24 00:29:11 -0400

    Task 1 to 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,33 +11,49 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/',function (req, res) {  
+  res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn
+  res.send(books[isbn])
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author
+    const finalDetails = new Map()
+
+    for (isbn in books) {
+        if (books[isbn].author === author) {
+            finalDetails.set(isbn, books[isbn])
+        }
+    }
+
+    res.send(JSON.stringify(Object.fromEntries(finalDetails), null, 4))
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title
+    const finalDetails = new Map()
+
+    for (isbn in books) {
+        if (books[isbn].title === title) {
+            finalDetails.set(isbn, books[isbn])
+        }
+    }
+
+    res.send(JSON.stringify(Object.fromEntries(finalDetails), null, 4))
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn
+
+    res.send(books[isbn].reviews)
 });
 
 module.exports.general = public_users;
